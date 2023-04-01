55d06a07efd7dc4b056e8df8c5b7c0c6f0b5119f
Vishal Vaish <114350565+vishal-vaish@users.noreply.github.com>
2023-04-01 20:12:36 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,49 +7,48 @@ const App = () => {
   const [isloading, setIsLoading] = useState(true);
 
   useEffect(()=>{
-    setIsLoading(true);
-    fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=[API_KEY]&max=10&lang=en`)
-    .then(res=>res.json())
-    .then(data=>{
-      setNews(data.articles);
+    // setIsLoading(true);
+    async function fetchNews(){
+      try {
+        setIsLoading(true);
+      const response=await fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=[API_KEY]&max=10&lang=en`);
+    const data=await response.json();
+    setNews(data.articles);
       setIsLoading(false);
-    })
-    .catch(err=> console.log(err));
-  },[category]);
+    }catch(err){
+      console.log(err);
+  }
+}
+fetchNews();
+},[category]);
 
   const handleCategoryChange=(event)=>{
     setCategory(event.target.value);
   }
 
   return (
-    <div id="main">
+    <div id="main" className="App">
       <h1 className='heading'>Top 10 {category} news.</h1>
-      <div>
-        <label htmlFor="category">Select a category:</label>
-      <select id="category" name="category" value={category} onChange={handleCategoryChange}>
+      <select value={category} onChange={handleCategoryChange}>
         <option value="general">General</option>
         <option value="business">Business</option>
-        <option value="entertainment">Entertainment</option>
-        <option value="health">Health</option>
+        <option value="technology">Technology</option>
         <option value="science">Science</option>
         <option value="sports">Sports</option>
-        <option value="technology">Technology</option>
+        <option value="entertainment">Entertainment</option>
       </select>
-      </div>
       {isloading ? (
-      <p className='loader'>Loading...</p>):(
-      <ol>
+      <p className='loader'>Loading...</p>
+      ):(
+        <div className="news-list">
         {news.map((article)=>{
-          <li key={article.url}>
-             <img src={article.image} alt={article.title}/>
-             <div>
-             <h2>{article.title}</h2>
-             <p>{article.description}</p>
-             <p>Source: {article.source.name}</p>
-             </div>
-           </li>
+          <div key={article.url} className="news-item">
+            <img src={article.image} alt={article.title} />
+            <h2>{article.title}</h2>
+            <p>{article.description}</p>
+            </div>
         })}
-      </ol>
+        </div>
   )}
     </div>
   )
